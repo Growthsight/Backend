ad11eb811f56c6ae282fbacab79cfda1ddae4443
Anubhav Gupta <anubhavg@bu.edu>
2016-04-07 11:22:30 -0400

    Temperature, Humidity and Precipitation Working

--- data_extract.py
+++ data_extract.py
@@ -32,41 +32,41 @@ if sys.argv[3] == 'winterwheat':
 			for j in range(0,24):
 				day_sum_temp = day_sum_temp + byHour.data[j].temperature
 				day_sum_hum = day_sum_hum + byHour.data[j].humidity*100
-				'''try:
+				try:
 					day_sum_precip = day_sum_precip + byHour.data[j].precipIntensity
 				except AttributeError:
-					error = error + 1'''
+					error = error + 1
 			day_avg_temp = day_sum_temp/24
 			day_avg_hum = day_sum_hum/24
-			'''if day_sum_precip == 0:
+			if day_sum_precip == 0:
 				day_avg_precip = 0
 			else:
-				day_avg_precip = day_sum_precip/(24-error)'''
+				day_avg_precip = day_sum_precip/(24 - error)
 			day_sum_temp = 0
 			day_sum_hum = 0
-			#day_sum_precip = 0
-			#error = 0
+			day_sum_precip = 0
+			error = 0
 			month_sum_temp = month_sum_temp + day_avg_temp
 			month_sum_hum = month_sum_hum + day_avg_hum
-			#month_sum_precip = month_sum_precip + day_avg_precip
+			month_sum_precip = month_sum_precip + day_avg_precip
 		month_avg_temp = month_sum_temp/28
 		month_avg_hum = month_sum_hum/28
-		'''if month_sum_precip == 0:
+		if month_sum_precip == 0:
 			month_avg_precip = 0
 		else:
-			month_avg_hum = month_sum_precip/28'''
+			month_avg_precip = month_sum_precip/28
 		month_sum_temp = 0
 		month_sum_hum = 0
-		#month_sum_precip = 0
+		month_sum_precip = 0
 		optimal_sum_temp = optimal_sum_temp + month_avg_temp
 		optimal_sum_hum = optimal_sum_hum + month_avg_hum
-		#optimal_sum_precip = optimal_sum_precip + month_avg_precip
+		optimal_sum_precip = optimal_sum_precip + month_avg_precip
 	optimal_avg_temp = optimal_sum_temp/3
 	optimal_avg_hum = optimal_sum_hum/3
-	'''if optimal_sum_precip == 0:
+	if optimal_sum_precip == 0:
 		optimal_avg_precip = 0
 	else:
-		optimal_avg_precip = optimal_sum_precip/3'''
+		optimal_avg_precip = optimal_sum_precip/3
 print optimal_avg_temp
 print optimal_avg_hum
-#print optimal_avg_precip
+print optimal_avg_precip
